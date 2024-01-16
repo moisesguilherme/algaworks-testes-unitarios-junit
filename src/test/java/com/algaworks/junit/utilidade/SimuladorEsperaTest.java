f2bab91c721710d69c66cb4d94bba0534c43fd7a
Moisés Guilherme <moisescomp@gmail.com>
2024-01-16 07:59:25 -0300

    2.10. Ignorando execução dos testes condicionalmente com Assumptions

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,30 +1,28 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 class SimuladorEsperaTest {
 
+
+    //@Disabled("Não é mais aplicável")
+    //@EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     @Test
-    @Disabled("Não é mais aplicável")
     void deveEsperarENaoDarTimeout() {
 
-        // Espera os 10 segundos
-        //Assertions.assertTimeout(Duration.ofSeconds(1),
-        //        () -> SimuladorEspera.esperar(Duration.ofSeconds(10)));
-
-        // Não vai esperar os 10 segundos, se passar de 1 já vai dar erro
+        //System.setProperty("ENV", "PROD");
+        //System.out.println("PROD".equals(System.getenv("ENV")));
+        Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")), () -> "Abortado teste: Não deve ser executado em PROD");
         Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1),
                 () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
-
-        // FIRST -
-        // Primeiro F (FAST)
-        // Fast: Testes unitários deven ser rápidos (Rodar em segundos ou menos)
-
     }
 }
