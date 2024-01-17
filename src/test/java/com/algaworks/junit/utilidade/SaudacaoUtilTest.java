e0e17751fd4607581563b34c52fb8672edf116fe
Moisés Guilherme <moisescomp@gmail.com>
2024-01-17 07:28:18 -0300

    2.12. Desafio - Escrevendo testes faltantes do SaudacaoUtil

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 // precisa ter o sufixo Test, algumaCoisaTeste
 class SaudacaoUtilTest {
 
@@ -15,7 +16,21 @@ class SaudacaoUtilTest {
             assertEquals("Bom dia", saudacao, "Saudacão incorreta!");
         }
 
+
         @Test
+        public void deveSaudarBoaTarde() {
+            String saudacao = SaudacaoUtil.saudar(12);
+            assertEquals("Boa tarde", saudacao, "Saudação incorreta");
+        }
+
+        @Test
+        public void deveSaudarBoaNoite() {
+            String saudacao = SaudacaoUtil.saudar(18);
+            assertEquals("Boa noite", saudacao, "Saudação incorreta");
+        }
+
+
+    @Test
         public void deveLancarException() {
             //SaudacaoUtil.saudar(-10); // erro
             //Executando o Debug com teste (Colocar o break point)
