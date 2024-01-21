95afc39114b615a834c5afba813423596ac45a18
Moisés Guilherme <moisescomp@gmail.com>
2024-01-21 19:56:36 -0300

    3.2. Alterando nome de exibição dos testes com @DisplayName

--- 03.01-organizando-testes-com-o-padrao-tripe-a/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ 03.01-organizando-testes-com-o-padrao-tripe-a/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,14 +1,17 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testes no utilitário de saudação")
 class SaudacaoUtilTest {
 
     @Test
+    @DisplayName("Deve saudar com bom dia")
     public void saudarComBomDia() {
         //Arrange
         int horaValida = 9;
@@ -21,6 +24,7 @@ class SaudacaoUtilTest {
     }
 
     @Test
+    @DisplayName("Deve saudar com bom dia às 5 horas")
     public void saudarComBomDiaAPartir5h() {
         int horaValida = 5;
         String saudacao = saudar(horaValida);
